a9b47ea3d9d2e3c44573ccbd7429e8086ce5f696
zackbensted <zackbensted@hotmail.co.uk>
2015-03-24 13:07:33 +0000

    Create leftnav.php

---
+++ leftnav.php
@@ -0,0 +1,5 @@
+  <a href="index.html">Processors</a>
+  <a href="index.html">RAM</a>
+  <a href="index.html">Hard Drives</a>
+  <a href="index.html">Graphics Cards</a>
+  <a href="index.html">Motherboards</a>
