73d7e2cb8503d048e26a7f251cd3264e6cbac9fa
zackbensted <zackbensted@hotmail.co.uk>
2015-03-24 13:08:36 +0000

    Delete leftnav.php

--- leftnav.php
+++
@@ -1,5 +0,0 @@
-  <a href="index.html">Processors</a>
-  <a href="index.html">RAM</a>
-  <a href="index.html">Hard Drives</a>
-  <a href="index.html">Graphics Cards</a>
-  <a href="index.html">Motherboards</a>
